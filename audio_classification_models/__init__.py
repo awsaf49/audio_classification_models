e57d87dd420ec6bd92486116c5a0a513afd20e4c
Awsaf <awsaf49@gmail.com>
2022-06-23 22:53:13 +0600

    update for kaggle

--- audio_classification_models/__init__.py
+++ audio_classification_models/__init__.py
@@ -1,2 +1,4 @@
 from .models.conformer import ConformerEncoder, Conformer
 from .models.contextnet import ContextNetEncoder, ContextNet
+from .utils.weights import load_pretrain
+from .version import __version__
